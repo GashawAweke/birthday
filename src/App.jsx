b8c98371df05839dd19a6b778ed3d04ca77b7b67
Gashaw <gashaye12aweke@gmail.comm>
2025-01-16 19:35:56 +0300

    first commit on birthday project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './app.css';
+import data from './data';
+import List from './List';
+function App() {
+  return (
+    <div className='container'>
+      <h2>reminder project setup</h2>;
+    </div>
+  );
+}
+
+export default App;
